3bc80ec8a4184778f6ab63193ec197c0caaa5d1b
idk <51036884+AcidicSoil@users.noreply.github.com>
2021-08-25 10:17:38 -0500

    Update hpPartNumberTesting.py

--- hpPartNumberTesting.py
+++ hpPartNumberTesting.py
@@ -1,3 +1,7 @@
+#https://www.geeksforgeeks.org/scrape-table-from-website-using-python-selenium/
+#https://selenium-python.readthedocs.io/navigating.html#popup-dialogs
+#https://selenium-python.readthedocs.io/waits.html
+#https://www.scrapingbee.com/blog/selenium-python/
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
